c4d0dc6477c4a3d693355fd6a8bb13e03dc39587
Saad <s.alajlan10@gmail.com>
2019-11-10 23:49:59 +0300

    LogInController

--- OnTheMap/Controller/LoginViewController.swift
+++ OnTheMap/Controller/LoginViewController.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2019 saad. All rights reserved.
 //
 import UIKit
-import Foundation
+
 
 class LoginViewController: UIViewController {
 
@@ -21,9 +21,51 @@ class LoginViewController: UIViewController {
         emailTextField.text = ""
         passwordTextField.text = ""
     }
-    @IBAction func loginTapped(_ sender: UIButton) {
+    @IBAction func loginTapped(_ sender: Any) {
        setLoggingIn(true)
+          let username = emailTextField.text!
+              let password = passwordTextField.text!
+              
+              if (username.isEmpty) || (password.isEmpty) {
+                  
+                  let requiredInfoAlert = UIAlertController (title: "Fill the required fields", message: "Please fill both the email and password", preferredStyle: .alert)
+                  
+                  requiredInfoAlert.addAction(UIAlertAction (title: "OK", style: .default, handler: { _ in
+                      return
+                  }))
+                  
+                  self.present (requiredInfoAlert, animated: true, completion: nil)
+                  
+              } else {
+                
+                ParseClient.postSession(email: username, password: password) { (result, error) in
+                    if let error = error {
+                        let errorAlert = UIAlertController(title: "Erorr performing request", message: error.localizedDescription, preferredStyle: .alert )
+                        
+                        errorAlert.addAction(UIAlertAction (title: "OK", style: .default, handler: { _ in
+                            return
+                        }))
+                                    }
+                    if let error = result?["error"] as? String {
+                        let errorAlert = UIAlertController(title: "Erorr performing request", message: error, preferredStyle: .alert )
+                        
+                        errorAlert.addAction(UIAlertAction (title: "OK", style: .default, handler: { _ in
+                            return
+                        }))
+                    }
+                    if let session = result?["session"] as? [String:Any], let sessionId = session["id"] as? String {
+                        print(sessionId)
+                        let controller = self.storyboard?.instantiateViewController(withIdentifier: "MapViewController") as! MapViewController
+                        
+
+                        self.navigationController?.pushViewController(controller, animated: true)
+                    }
+                    
         
+                                    
+    
+    }
+        }
     }
     func setLoggingIn(_ loggingIn: Bool) {
            if loggingIn {
